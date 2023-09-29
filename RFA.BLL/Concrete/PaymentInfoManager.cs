9cfbd13c55134ffd90ceee4262cd56b7d3f3742b
Mahmut Gülen <69367981+mahmutgulen@users.noreply.github.com>
2023-09-29 11:18:55 +0300

    bll

---
+++ RFA.BLL/Concrete/PaymentInfoManager.cs
@@ -0,0 +1,13 @@
+﻿using RFA.BLL.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFA.BLL.Concrete
+{
+    public class PaymentInfoManager : IPaymentInfoService
+    {
+    }
+}
--- RFA.BLL/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ RFA.BLL/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -1,4 +1,6 @@
 ﻿using Autofac;
+using RFA.BLL.Abstract;
+using RFA.BLL.Concrete;
 using RFA.DAL.Abstract;
 using RFA.DAL.Concrete;
 
@@ -8,6 +10,10 @@ namespace RFA.BLL.DependencyResolvers.Autofac
     {
         protected override void Load(ContainerBuilder builder)
         {
+            builder.RegisterType<RegistrationInfoManager>().As<IRegistrationInfoService>();
+            builder.RegisterType<PaymentInfoManager>().As<IPaymentInfoService>();
+
+
             builder.RegisterType<RegistrationInfoDal>().As<IRegistrationInfoDal>();
             builder.RegisterType<PaymentInfoDal>().As<IPaymentInfosDal>();
         }
